b3d43b958cfea5553233159ddcf833a61aca67dc
Harim-Yoo <jamesyhr@gmail.com>
2025-12-16 22:55:33 +0900

    more practicing on react

--- 251216_project2/src/App.jsx
+++ 251216_project2/src/App.jsx
@@ -1,26 +1,23 @@
 import { useState } from 'react'
 
 function App() {
-  const [order, setOrder] = useState({ name: "메뉴를 정해주세요", price: 0 });
-  const menuList = [
-    { name: "피자", price: 10000 },
-    { name: "치킨", price: 20000 },
-    { name: "샐러드", price: 15000 }
-  ]
+  const [myAnswer, setMyanswer] = useState("");
+  const [result, setResult] = useState("");
+  const correctAnswer = "4";
+
+  const checkAnswer = () => {
+    (myAnswer===correctAnswer) ? setResult("정답입니다") : setResult("틀렸습니다.")
+  }
   return (
     <div>
-    <div> 오늘의 메뉴: { order.name } with { order.price }</div >
-  {
-    menuList.map((item) => (
-      <button
-        key={item.name}
-        onClick={() => setOrder(item)}>
-        {item.name}
-      </button>
-    ))
-  }
-  </div>
-)
+      <h1>문제: 2+2 =? </h1>
+      <input 
+        onChange={(event)=>setMyanswer(event.target.value)}
+        value={myAnswer}
+        placeholder="정답을 입력하세요."/>
+      <button onClick={checkAnswer}>제출하기</button>
+      <p>{result}</p>
+    </div>
+  )
 }
-
 export default App
