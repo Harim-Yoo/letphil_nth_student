51b4e9c6b4a7bf7e597481875fea7d3734f09e9e
Harim Yoo <jamesyhr@gmail.com>
2025-12-17 22:51:53 +0900

    practing on post method

--- 251217_project/index.js
+++ 251217_project/index.js
@@ -3,21 +3,48 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-
 const port = 3000;
+const satQuestions = [
+        { 
+            id: 1, 
+            text: "If 2x + 3 = 9, what is x?", 
+            options: ["1", "2", "3", "4"], 
+            answer: "3" 
+        },
+        { 
+            id: 2, 
+            text: "Which implies x > 0?", 
+            options: ["x+1=0", "x-1=0", "x=0", "x=-5"], 
+            answer: "x-1=0" 
+        }
+    ];
+
 app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,"/public/index.html"));
 })
+
 app.get('/test', (req,res) => {
-    const dataArray = [
-        { name: "Alice", age: 26 },
-        { name: "Bob", age: 25 },
-        { name: "Charlie", age: 24 }
-    ]
-    res.json(dataArray); //stringify the data
+    res.json(satQuestions); //stringify the data
     });
+
+app.post('/check', (req,res) => {
+    const dataForCheck = req.body;
+    const problemId = dataForCheck.id;
+    const answerGuess = dataForCheck.guess;
+    const problem = satQuestions.find(p => p.id === problemId);
+    if (!problem) {
+        return res.status(404).json( { error : "Problem not found"});
+    }
+    if (answerGuess === problem.answer) {
+        res.json( { correct: true } );
+    } else {
+        res.json( { correct: false } );
+    }
+})
 
 app.listen(port, () => {
     console.log(`http://localhost:${port} is now open.`)
