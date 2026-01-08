7d3a819de7300cba58e6a062179218afe185fa32
Harim-Yoo <jamesyhr@gmail.com>
2026-01-08 22:05:55 +0900

    doin homework

--- Js-Fundamentals/09_DOM_EventsMiniTasks/mentee/homework.js
+++ Js-Fundamentals/09_DOM_EventsMiniTasks/mentee/homework.js
@@ -11,19 +11,24 @@
 //         - The mood select (id "moodSelect")
 //         - The reset button (id "resetProfileBtn")
 
-// STEP 2: Add an "input" event listener to the name input.
-//         Inside the listener:
-//         - Read the current input value.
-//         - If it's empty, set the display name to "Your Name".
-//         - Otherwise, set the display name to the typed value.
+const profileNameDisplay = document.getElementById('profileNameDisplay');
+const profileMoodDisplay = document.getElementById('profileMoodDisplay');
+const colorValueText = document.getElementById('colorValueText');
+const nameInput = document.getElementById('nameInput');
+const moodSelect = document.getElementById('moodSelect');
+const resetProfileBtn = document.getElementById('resetProfileBtn');
 
-// STEP 3: Add an "input" event listener to the color input.
-//         Inside the listener:
-//         - Read the current input value.
-//         - If it's empty, set the color text to "none yet".
-//         - Otherwise, set the color text to the typed value.
-//         (Optional) You can later also change the text color itself,
-//         but for now just update the text.
+nameInput.addEventListener('input',()=>{
+    const inputVal = nameInput.value;
+    inputVal.trim() === "" ? profileNameDisplay.textContent = "Your Name" : profileNameDisplay.textContent = inputVal;
+})
+
+colorInput.addEventListener('input',()=>{
+    const inputVal = colorInput.value;
+    inputVal.trim() === "" ? colorValueText.textContent = "none yet" : colorValueText.textContent = inputVal
+    colorValueText.style.color = inputVal; // I did it for extra fun. This is great.
+});
+
 
 // STEP 4: Add a "change" event listener to the mood select.
 //         Inside the listener:
