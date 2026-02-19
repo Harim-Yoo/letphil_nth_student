def6a2325d91b0b05efeb38383a432832b4f3b09
Harim Yoo <jamesyhr@gmail.com>
2026-02-19 11:39:58 +0900

    starting level3

---
+++ Lv3-React/korea/week-1/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { CountOccurences } from "./reducing/CountOccurences";
+
+function App() {
+  return (
+    <div style={{ color: "black", backgroundColor: "gray" }}>
+      <CountOccurences />
+    </div>
+  );
+}
+
+export default App;
